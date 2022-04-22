280982aaf9fc3ebc2e9803ad8916bb24793ba669
voloide <voloidet@gmail.com>
2022-04-22 10:33:16 +0200

    Estruturação da componente de parametros

--- iDMED/src/components/Reports/shared/FiltersInput.vue
+++ iDMED/src/components/Reports/shared/FiltersInput.vue
@@ -1,5 +1,6 @@
 <template>
-             <div class="row ">
+             <div class="row">
+              <div class="col row q-pt-md">
                 <q-select
                     dense outlined
                     class="col q-mr-md"
@@ -7,7 +8,6 @@
                     v-model="province"
                     ref="province"
                     option-value="id"
-                    style="max-width: 170px"
                     option-label="description"
                     :rules="[ val => ( val != null ) || ' Por favor indique a província']"
                     lazy-rules
@@ -21,7 +21,6 @@
                     ref="district"
                     option-value="id"
                     option-label="description"
-                    style="max-width: 150px"
                     :rules="[ val => ( val != null) || ' Por favor indique o Distrito']"
                     lazy-rules
                     label="Distrito *" />
@@ -34,7 +33,6 @@
                     ref="clinic"
                     option-value="id"
                     option-label="clinicName"
-                    style="max-width: 220px"
                     :rules="[ val => ( val != null) || ' Por favor indique a farmácia']"
                     lazy-rules
                     label="Farmácia *" />
@@ -44,7 +42,6 @@
                     dense outlined
                     :options="periodos"
                     v-model="periodo"
-                   style="max-width: 140px"
                     @update:model-value="val => onPeriodoChange(val)"
                     ref="period"
                     option-value="id"
@@ -53,79 +50,110 @@
                     lazy-rules
                     label="Período *" />
 
-                        <SpecificPeriod v-if="periodo.id ===1"/>
-                        <MonthlyPeriod v-model="month" v-else-if="periodo.id ===2" />
-                        <QuarterlyPeriod v-else-if="periodo.id ===3" />
-                        <SemesterPeriod v-else-if="periodo.id ===4" />
-                        <AnnualPeriod v-else-if="periodo.id ===5" />
+                    <SpecificPeriod v-model="thruDate" v-if="periodo.id ===1"/>
+                    <MonthlyPeriod v-model="month" v-else-if="periodo.id ===2" />
+                    <QuarterlyPeriod v-else-if="periodo.id ===3" />
+                    <SemesterPeriod v-else-if="periodo.id ===4" />
+                    <AnnualPeriod v-else-if="periodo.id ===5" />
 
-                        <div class="" >
-                        <q-btn class="flat rounded"
-                        color="grey"
-                        size ="sm"
-                        rounded
-                        icon="chevron_right"
-                        @click.stop="processReport(id,typeService)"
-                            >
-                            <q-tooltip class="bg-primary">processar o relatório</q-tooltip>
-                            </q-btn>
+                    <div class="">
+                    <q-btn class="gt-xs"
+                      color="grey"
+                      dense
+                      rounded
+                      icon="chevron_right"
+                      @click.stop="processReport(id,typeService)"
+                    >
+                      <q-tooltip class="bg-primary">Processar o Relatório</q-tooltip>
+                    </q-btn>
+                    </div>
+                    <div class="q-mr-md q-pt-xs items-center" style="width: 250px;">
+                      <q-linear-progress class="col q-mx-md" size="25px" stripe rounded :value="progress1" color="red">
+                        <div class="absolute-full flex flex-center">
+                            <q-badge color="white" text-color="accent" :label="progressLabel1" />
                         </div>
+                      </q-linear-progress>
+                    </div>
+              </div>
+              <div class="row q-ml-md">
+                <div class="col">
+                  <q-btn color="grey-6" class="row gt-xs" flat dense icon="article" @click.stop="generateReport('XLS')">
+                    <q-tooltip class="bg-primary">Imprimir Excel</q-tooltip>
+                    .Xls
+                </q-btn>
+                <q-btn color="grey-6" class="gt-xs" flat dense  @click.stop="generateReport('PDF')" icon="article" title=".pdf">
+                  <q-tooltip class="bg-primary">Imprimmir Pdf</q-tooltip>
+                  .Pdf
+                  </q-btn>
                 </div>
+              </div>
+            </div>
 </template>
 
 <script>
 import Province from '../../../store/models/province/Province'
 import District from '../../../store/models/district/District'
 import Clinic from '../../../store/models/clinic/Clinic'
+import { ref, computed } from 'vue'
 
 export default {
-    props: ['typeService', 'menuSelected', 'id'],
+    props: ['typeService', 'menuSelected', 'id', 'totalRecords', 'qtyProcessed'],
     data () {
-            return {
-                province: null,
-                district: null,
-                clinic: null,
-                periodo: '',
-                periodos: [
-                    { id: 1, description: 'Especifico' },
-                    { id: 2, description: 'Mensal' },
-                    { id: 3, description: 'Trimestral' },
-                    { id: 4, description: 'Semestral' },
-                    { id: 5, description: 'Anual' }]
-            }
+      const progress1 = ref(0.25)
+      return {
+          province: null,
+          district: null,
+          clinic: null,
+          thruDate: ref(''),
+          startDate: ref(''),
+          name: ref(''),
+          periodo: '',
+          periodos: [
+              { id: 1, description: 'Especifico' },
+              { id: 2, description: 'Mensal' },
+              { id: 3, description: 'Trimestral' },
+              { id: 4, description: 'Semestral' },
+              { id: 5, description: 'Anual' }],
+          progress1,
+          progressLabel1: computed(() => (progress1.value * 100).toFixed(2) + '%')
+      }
     },
     computed: {
-          provinces () {
-            return Province.query().with('districts').has('code').get()
-        },
-        districts () {
-            if (this.province !== null) {
-            console.log('Provincia', this.province)
-            return District.query().with('clinics').where('province_id', this.province.id).get()
-            } else {
-                return null
-            }
-        },
-         clinics () {
-            if (this.district !== null) {
-            console.log('district', this.district)
-            return Clinic.query().with('districts').where('district_id', this.district.id).get()
-            } else {
-                return null
-            }
+      provinces () {
+        return Province.query().with('districts').has('code').get()
+      },
+      districts () {
+        if (this.province !== null) {
+          return District.query().with('clinics').where('province_id', this.province.id).get()
+        } else {
+            return null
         }
+      },
+        clinics () {
+          if (this.district !== null) {
+            return Clinic.query().with('districts').where('district_id', this.district.id).get()
+          } else {
+              return null
+          }
+      }
     },
     methods: {
-    processReport (id, typeService) {
-        console.log('Processando o report UUID', id)
-        console.log('Tipo de Servico', typeService.code)
-        console.log('Data inicio', this.startDate)
-        console.log('Data fim', this.thruDate)
-        console.log('Mes', this.month)
-    },
-    onPeriodoChange (val) {
-         console.log('Valor seleccionado: ', val)
-    }
+      processReport (id, typeService) {
+          console.log('Processando o report UUID', id)
+          console.log('Tipo de Servico', typeService.code)
+          console.log('Data inicio', this.startDate)
+          console.log('Data fim', this.thruDate)
+          console.log('Mes', this.month)
+      },
+      onPeriodoChange (val) {
+          console.log('Valor seleccionado: ', val)
+      },
+      generateReport (fileType) {
+        this.$emit('getReport', this.id, fileType)
+      },
+      initReportProcessing () {
+        this.$emit('initReportProcessing', this.id)
+      }
     },
     components: {
        SpecificPeriod: require('components/Reports/shared/SpecificPeriod.vue').default,
