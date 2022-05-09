9bdf29f6c59e4ee9fd9e1de97fb88a9c54095980
muhammadchona <muhammadchona@gmail.com>
2022-05-09 10:59:07 +0200

    uniformizacao filter input relatorios gestao de referencias

---
+++ iDMED/src/components/Reports/ReferralManagement/ReferredPatients.vue
@@ -0,0 +1,108 @@
+<template>
+<div ref="filterDrugStoreSection">
+  <ListHeader
+    :addVisible="false"
+    :mainContainer="true"
+    :closeVisible="true"
+    @closeSection="closeSection"
+    bgColor="bg-orange-5">Serviço {{selectedService !== null ? selectedService.code : ''}}: Referidos Para Outras Farmacias
+  </ListHeader>
+  <div class="param-container">
+    <q-item>
+        <q-item-section  class="col" >
+            <FiltersInput
+              :id="id"
+              :clinicalService="selectedService"
+              :totalRecords="totalRecords"
+              :qtyProcessed="qtyProcessed"
+              :reportType="report"
+              @generateReport="generateReport"
+              @initReportProcessing="initReportProcessing"
+            />
+        </q-item-section>
+    </q-item>
+  </div>
+  </div>
+</template>
+
+<script>
+
+import Pack from 'src/store/models/packaging/Pack'
+import { ref } from 'vue'
+  export default {
+    name: 'ReferredPatients',
+    props: ['selectedService', 'menuSelected', 'id'],
+    setup () {
+      return {
+        totalRecords: ref(0),
+        qtyProcessed: ref(0),
+        report: 'REFERIDO_PARA'
+      }
+    },
+    mounted () {
+    },
+    components: {
+      ListHeader: require('components/Shared/ListHeader.vue').default,
+      FiltersInput: require('components/Reports/shared/FiltersInput.vue').default
+    },
+    methods: {
+      closeSection () {
+        this.$refs.filterDrugStoreSection.remove()
+      },
+      initReportProcessing (params) {
+        console.log(params)
+      Pack.api().post('/referredPatientsReport/initReportProcess', params)
+      },
+      generateReport (id, pdf) {
+        // UID da tab corrente
+        console.log('UUID da tab seleccionada:', id)
+       // console.log(Pack.api().get('/referredPatientsReport/printReport/'+ id).toString)
+            Pack.api().get('/referredPatientsReport/printReport/' + id,
+            { responseType: 'blob' }).then(resp => {
+              console.log(resp)
+              console.log(resp.response.data)
+                const file = new Blob([resp.response.data], { type: 'application/pdf' })
+        const fileURL = URL.createObjectURL(file)
+          const link = document.createElement('a')
+          link.href = fileURL
+          link.setAttribute('download', 'file.pdf')
+          document.body.appendChild(link)
+          link.click()
+            })
+      }
+              //  const bytes = btoa(new Uint8Array(resp.data).reduce((data, byte) => data + String.fromCharCode(byte), ''))
+             //  const url = 'data:application/pdf;base64, ' + bytes
+        /*    const link = document.createElement('a')
+             const downloadUrl = window.URL.createObjectURL(new Blob([resp.response.data]))
+            link.href = downloadUrl
+            link.setAttribute('download', 'file.pdf')
+           document.body.appendChild(link)
+            link.click()
+            link.remove() */
+        /*   axios({
+              url: 'http://localhost:8884/report/',
+            method: 'GET',
+          responseType: 'blob' // important
+             }).then((resp) => {
+                const file = new Blob([resp.data], { type: 'application/pdf' })
+      const fileURL = URL.createObjectURL(file)
+        const link = document.createElement('a')
+        link.href = fileURL
+         link.setAttribute('download', 'file.pdf')
+        document.body.appendChild(link)
+        link.click()
+             }) */
+    //  }
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .param-container {
+    border-bottom: 1px dashed $grey-13;
+    border-left: 1px dashed $grey-13;
+    border-right: 1px dashed $grey-13;
+    border-radius: 0px 0px 5px 5px;
+  }
+</style>
