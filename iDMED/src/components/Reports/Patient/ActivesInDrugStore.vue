6d12f0905f24ccace804dfb18437d41097c13f4d
Joao Nuno Mabjaia <94103183+joaonunomabjaia@users.noreply.github.com>
2022-06-16 16:50:35 +0200

    PDF & EXCEL Active Report Done

--- iDMED/src/components/Reports/Patient/ActivesInDrugStore.vue
+++ iDMED/src/components/Reports/Patient/ActivesInDrugStore.vue
@@ -32,11 +32,10 @@
 </template>
 
 <script>
-import JsPDF from 'jspdf'
-import autoTable from 'jspdf-autotable'
 import moment from 'moment'
 import Report from 'src/store/models/report/Report'
 import { LocalStorage } from 'quasar'
+import activePatients from '../../../reports/Patients/ActivePatients.ts'
 import { ref } from 'vue'
   export default {
     name: 'DrugStore',
@@ -88,20 +87,16 @@ import { ref } from 'vue'
       generateReport (id, fileType) {
           Report.api().get(`/activePatientReport/printReport/${id}`, { responseType: 'json' }).then(resp => {
             const patientAux = resp.response.data[0]
-          console.log(patientAux.province)
 
           if (fileType === 'PDF') {
-            console.log(fileType)
-            this.downloadPDF(
-              null,
+            activePatients.downloadPDF(
               patientAux.province,
               moment(new Date(patientAux.startDate)).format('DD-MM-YYYY'),
               moment(new Date(patientAux.endDate)).format('DD-MM-YYYY'),
               resp.response.data
             )
           } else {
-            this.downloadExcel(
-              null,
+            activePatients.downloadExcel(
               patientAux.province,
               moment(new Date(patientAux.startDate)).format('DD-MM-YYYY'),
               moment(new Date(patientAux.endDate)).format('DD-MM-YYYY'),
@@ -110,172 +105,6 @@ import { ref } from 'vue'
           }
         })
       },
-      // generateReport (id, fileType) {
-      //   // UID da tab corrente
-      //    Report.api().get(`/activePatientReport/printReport/${id}/${fileType}`, { responseType: 'blob' }).then(resp => {
-      //     const file = new Blob([resp.response.data], { type: 'application/pdf' })
-      //     const fileURL = URL.createObjectURL(file)
-      //     const link = document.createElement('a')
-      //     link.href = fileURL
-      //     link.setAttribute('download', 'ActivePatientReport.' + fileType)
-      //     document.body.appendChild(link)
-      //     link.click()
-      //   })
-      // },
-      async downloadExcel (facility, province, startDate, endDate, params) {
-        alert('Excel')
-      },
-      async downloadPDF (facility, province, startDate, endDate, result) {
-        const doc = new JsPDF({
-          orientation: 'l',
-          unit: 'mm',
-          format: 'a4',
-          putOnlyUsedFonts: true,
-          floatPrecision: 'smart' // or "smart", default is 16
-        })
-        const firstObject = result[0]
-        const totalPagesExp = '{total_pages_count_string}'
-        /*
-          Fill Table
-        */
-
-       const desiredDefinition = [
-      [
-        {
-          content: '                                                                                                        Lista de Pacientes Activos na Farmácia',
-          colSpan: 3,
-          halign: 'center',
-          valign: 'middle',
-          fontStyle: 'bold',
-          fontSize: '14'
-        }
-      ],
-      [
-        {
-          content: 'US: ' + firstObject.clinic,
-          colSpan: 2,
-          halign: 'center',
-          valign: 'middle',
-          fontStyle: 'bold',
-          fontSize: '14'
-        },
-        {
-          content: 'Periodo: ' + startDate + ' à ' + endDate,
-          colSpan: 1,
-          halign: 'center',
-          valign: 'middle',
-          fontStyle: 'bold',
-          fontSize: '14'
-        }
-      ],
-      [
-        {
-          content: 'Distrito: ' + firstObject.district,
-          halign: 'center',
-          valign: 'middle',
-          fontStyle: 'bold',
-          fontSize: '14'
-         },
-        {
-          content: 'Provincia: ' + province,
-          halign: 'center',
-          valign: 'left',
-          fontStyle: 'bold',
-          fontSize: '14'
-         },
-        {
-          content: 'Ano: ' + firstObject.year,
-          halign: 'center',
-          valign: 'left',
-          fontStyle: 'bold',
-          fontSize: '14'
-         }
-      ]
-    ]
-    autoTable(
-      doc,
-
-      {
-        margin: { top: 20 },
-        bodyStyles: {
-          halign: 'left',
-          valign: 'middle'
-        },
-        headStyles: {
-          halign: 'center',
-          valign: 'middle'
-        },
-
-        theme: 'grid',
-        body: desiredDefinition
-      }
-    )
-
-        const cols = [
-          'ORD',
-          'NID',
-          'Nome',
-          'Idade',
-          'Contacto',
-          'Tipo Paciente',
-          'Linha Terapeutica',
-          'Regime Terapeutico',
-          'Data Levant.',
-          'Data Prox. Levant.'
-        ]
-
-        const rows = result
-        const data = []
-        let ord = 1
-
-        for (const row in rows) {
-          const createRow = []
-          createRow.push(ord)
-          createRow.push(rows[row].nid)
-          createRow.push(rows[row].firstNames + ' ' + rows[row].middleNames + ' ' + rows[row].lastNames)
-          createRow.push(rows[row].age)
-          createRow.push(rows[row].cellphone)
-          createRow.push(rows[row].patientType)
-          createRow.push(rows[row].therapeuticLine)
-          createRow.push(rows[row].therapeuticRegimen)
-          createRow.push(moment(new Date(rows[row].pickupDate)).format('DD-MM-YYYY'))
-          createRow.push(moment(new Date(rows[row].nextPickUpDate)).format('DD-MM-YYYY'))
-
-          data.push(createRow)
-          ord += 1
-        }
-        ord = 0
-        autoTable(doc, {
-          margin: { top: 20 },
-          bodyStyles: {
-            halign: 'center'
-          },
-          headStyles: {
-            halign: 'center',
-            valign: 'middle'
-          },
-          didDrawPage: function (data) {
-            // Footer
-            let str = 'Pagina ' + doc.internal.getNumberOfPages()
-            // Total page number plugin only available in jspdf v1.0+
-            if (typeof doc.putTotalPages === 'function') {
-              str = str + ' de ' + totalPagesExp
-            }
-            doc.setFontSize(10)
-
-          // jsPDF 1.4+ uses getWidth, <1.4 uses .width
-          const pageSize = doc.internal.pageSize
-          const pageHeight = pageSize.height ? pageSize.height : pageSize.getHeight()
-          doc.text(str, data.settings.margin.left, pageHeight - 10)
-          },
-          startY: doc.lastAutoTable.finalY,
-          theme: 'grid',
-          head: [cols],
-          body: data
-        })
-        // params.value.loading.loading.hide()
-        return doc.save('PacientesActivos.pdf')
-  },
       displayAlert (type, msg) {
         this.alert.type = type
         this.alert.msg = msg
